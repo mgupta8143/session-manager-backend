9f56eb4fc5cdcf792f1d59fba29ead3840691dd0
mgupta8143 <mgupta8143@gmail.com>
2021-06-28 01:47:48 -0400

    Deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "https://session-manager.vercel.app/",
+        origin: "https://session-manager.vercel.app",
         methods: ["GET", "POST"]
     }
 });
