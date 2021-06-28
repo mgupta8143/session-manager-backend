f0507a7d39900fc5ceb3770966fb16b728bbfedc
mgupta8143 <mgupta8143@gmail.com>
2021-06-28 01:04:29 -0400

    Documentation

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ io.on('connection', socket => {
 
     socket.on("connect_session", (userData) => {
 
-        if(!(userData.sessionId in sessions)) {
+        //if the session hasn't been created or a name hasn't been inputted, go back to join page
+        if(!(userData.sessionId in sessions) || !userData.name) {
             socket.emit("session_not_available");
             return;
         } 
@@ -28,26 +29,43 @@ io.on('connection', socket => {
         socket.room = userData.sessionId;
         socket.join(socket.room);
 
+        //for the sockets session add a new participant 
         sessions[socket.room].participants[socket.id] = userData.name;
-        const participantList = getDictionaryValues(sessions[socket.room].participants)
+
+        //this takes O(n) time (could be reduced to O(1) but had no time)
+        const participantList = getDictionaryValues(sessions[socket.room].participants);
         
+        //update participant list
         io.to(socket.room).emit("update_participant_list", participantList);
     });
 
     socket.on('disconnect', () => {
-        delete sessions[socket.room].participants[socket.id];
-        const participantList = getDictionaryValues(sessions[socket.room].participants)
-        io.to(socket.room).emit("update_participant_list", participantList);
-        io.to(socket.room).emit("clear_local_storage");
+        if(sessions[socket.room]) {
+            //delete the participant from the dictionary
+            delete sessions[socket.room].participants[socket.id];
 
-        if(Object.keys(sessions[socket.room].participants).length === 0) {
-            delete sessions[socket.room];
+            //update the participant list on all clients
+            const participantList = getDictionaryValues(sessions[socket.room].participants)
+            io.to(socket.room).emit("update_participant_list", participantList);
+
+            //clear client local storage of name
+            io.to(socket.room).emit("clear_local_storage");
+
+            //if there are no participants in the session delete the session entirely
+            if(sessions[socket.room] && Object.keys(sessions[socket.room].participants).length === 0) {
+                delete sessions[socket.room];
+            }
         }
 
         socket.room = null;
     })
 });
 
+/**
+ * Returns the values of an object in list format
+ * @param {Object} dic 
+ * @returns {List} list 
+ */
 const getDictionaryValues = (dic) => {
     list = [];
     for(let key in dic) {
@@ -56,6 +74,11 @@ const getDictionaryValues = (dic) => {
     return list;
 }
 
+/**
+ * Creates a new session 
+ * @param {Object} sessionInfo 
+ * @returns {void}
+ */
 const createNewSession = (sessionInfo) => {
     if (!sessionInfo) return;
     if (sessionInfo.sessionId in sessions) return;
@@ -66,6 +89,7 @@ const createNewSession = (sessionInfo) => {
     };
 }
 
+// POST Request to create new session
 app.post("/create-session", (req, res) => {
     createNewSession(req.body);
     res.end();
