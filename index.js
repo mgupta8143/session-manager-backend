48cf75bc633e9f904e48be1dcf09fc6982950556
mgupta8143 <mgupta8143@gmail.com>
2021-06-28 01:34:57 -0400

    Vercel support added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://session-manager.vercel.app/",
         methods: ["GET", "POST"]
     }
 });
